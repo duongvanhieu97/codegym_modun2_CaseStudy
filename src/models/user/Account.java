6b1ce8405a9335547b1a8760f0884f9796076b88
Duonghieu97st <Duonghieu97st@gmail.com>
2022-09-07 14:19:36 +0700

    model acount chua hoan thien

--- src/models/user/Account.java
+++ src/models/user/Account.java
@@ -1,4 +1,40 @@
 package models.user;
 
-public class Account {
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private String user;
+    private String password;
+
+    public Account() {
+    }
+
+    public Account(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "user='" + user + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/storage/ReadWriteFile.java
+++ src/storage/ReadWriteFile.java
@@ -1,2 +1,25 @@
-package storage;public class ReadWriteFile {
+package storage;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class ReadWriteFile<E> {
+    public void WriteFileData(ArrayList<E> arrayList, String name){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(new File(name)));
+            objectOutputStream.writeObject(arrayList);
+            objectOutputStream.close();
+        }catch (IOException e) {
+            System.err.printf(e.getMessage());
+        }
+    }
+    public ArrayList<E> readFileData(String name) {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(name));
+            return (ArrayList<E>) objectInputStream.readObject();
+        }catch (IOException | ClassNotFoundException e) {
+            System.out.println();
+        }
+        return null;
+    }
 }
